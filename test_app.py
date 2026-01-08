6830bcc4e17a5557d12e3f32f4e16e35cafc7d3b
Maryam <maryamegadi@gmail.com>
2026-01-08 13:32:18 +0100

    Ajout des tests unitaires automatisés

--- test_app.py
+++ test_app.py
@@ -1,2 +1,13 @@
-def test_basic():
-    assert 1+1==2
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_home_page(client):
+    """Teste si la page d'accueil renvoie le bon message"""
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Hello DevOps CI/CD" in response.data
